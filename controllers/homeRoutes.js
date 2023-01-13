69cc4410ccb659f6a748da8c348980c606a36b95
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-01-12 16:23:37 -0800

    Added seeds, homepage views and controller starters

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { BlogPost, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
+
+        res.render('homepage', {
+            blogPosts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+});
+  
+module.exports = router;
