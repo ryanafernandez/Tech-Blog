dc918af010721586608a0abe672ea3d6317c93e7
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-01-28 16:25:19 -0800

    Added code for creating a new post

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const blogPostRoutes = require('./blogPostRoutes');
 const commentRoutes = require('./commentRoutes.js');
 
 router.use("/users", userRoutes);
-router.use("/blogpost", blogPostRoutes);
-router.use("/comment", commentRoutes);
+router.use("/blogposts", blogPostRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
